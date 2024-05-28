159b29e046de1255248553f9abeefef9512218de
eaintthayaphyoo <eaintthayaphyoo098@gmail.com>
2024-05-28 13:35:29 +0630

    category home page adding

---
+++ quiz-bot-frontend/src/pages/quiz/QuizHome.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const QuizHome = () => {
+  return (
+    <div>this is quiz page</div>
+  )
+}
---
+++ quiz-bot-frontend/src/pages/quiz/QuizLayout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const QuizLayout = () => {
+  return <Outlet />;
+};
+
+export default QuizLayout;
